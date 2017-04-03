c873f66231c3881756d02714e19a8cc3c274b95a
Aditya Karnik <aditya.s.karnik@gmail.com>
2017-04-03 19:15:39 +0530

    edit a record

--- app/src/main/java/com/example/aditya/realmexample/MainActivity.java
+++ app/src/main/java/com/example/aditya/realmexample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.aditya.realmexample;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,12 +10,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 import io.realm.Realm;
+import io.realm.RealmQuery;
 import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,13 +25,15 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     DataList dataListAdapter;
     List<DataObject> dataList = new ArrayList<>();
+    String idToEdit;
+    Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Realm realm = Realm.getInstance(MainActivity.this);
+        realm = Realm.getInstance(MainActivity.this);
 
 
         final EditText editText = (EditText) findViewById(R.id.edit_name);
@@ -37,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         final EditText editText3 = (EditText) findViewById(R.id.edit_email);
 
         Button button = (Button) findViewById(R.id.submit);
+        Button edit = (Button) findViewById(R.id.edit);
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
@@ -58,6 +64,84 @@ public class MainActivity extends AppCompatActivity {
 
             dataListAdapter.notifyDataSetChanged();
         }
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.enter_id);
+
+                final EditText editTextId = (EditText) dialog.findViewById(R.id.editTextId);
+                Button buttonOk = (Button) dialog.findViewById(R.id.buttonOk);
+                buttonOk.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        idToEdit = editTextId.getText().toString();
+                        /*RealmQuery<ObjectData> results1 =
+                                realm.where(ObjectData.class).equalTo("id",Integer.parseInt(idToEdit));
+                        Log.d("resultforquery", String.valueOf(realm.where(ObjectData.class).equalTo("id",Integer.parseInt(idToEdit))));*/
+                        if(getData(Integer.parseInt(idToEdit)) != null) {
+                            final Dialog dialog2 = new Dialog(MainActivity.this);
+                            dialog2.setContentView(R.layout.edit_the_details);
+                            final EditText editText = (EditText) dialog2.findViewById(R.id.edit_name);
+                            final EditText editText1 = (EditText) dialog2.findViewById(R.id.edit_age);
+                            final EditText editText2 = (EditText) dialog2.findViewById(R.id.edit_number);
+                            final EditText editText3 = (EditText) dialog2.findViewById(R.id.edit_email);
+                            final TextView textViewID = (TextView) dialog2.findViewById(R.id.textViewID);
+
+                            Button button = (Button) dialog2.findViewById(R.id.submit);
+                            textViewID.setText(String.valueOf(getData(Integer.parseInt(idToEdit)).getId()));
+                            editText.setText(getData(Integer.parseInt(idToEdit)).getName());
+                            editText1.setText(getData(Integer.parseInt(idToEdit)).getAge());
+                            editText2.setText(getData(Integer.parseInt(idToEdit)).getNumber());
+                            editText3.setText(getData(Integer.parseInt(idToEdit)).getEmail());
+
+                            button.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    realm.beginTransaction();
+                                    final ObjectData object = realm.createObject(ObjectData.class);
+
+                                    object.setId(getData(Integer.parseInt(idToEdit)).getId());
+                                    object.setName(editText.getText().toString());
+                                    object.setAge(editText1.getText().toString());
+                                    object.setNumber(editText2.getText().toString());
+                                    object.setEmail(editText3.getText().toString());
+
+                                    realm.commitTransaction();
+
+                                    clear();
+
+                                    RealmResults<ObjectData> results1 =
+                                            realm.where(ObjectData.class).findAll();
+                                    for(ObjectData c:results1) {
+                                        DataObject dataObject = new DataObject(c.getId(),
+                                                c.getName(),
+                                                c.getAge(),
+                                                c.getNumber(),
+                                                c.getEmail());
+                                        dataList.add(dataObject);
+
+                                        dataListAdapter.notifyDataSetChanged();
+                                    }
+                                }
+                            });
+
+                            dialog2.show();
+                            Log.d("resultforqueryname", getData(Integer.parseInt(idToEdit)).getName());
+                        } else {
+                            Toast.makeText(MainActivity.this, "No such ID exists", Toast.LENGTH_SHORT).show();
+                            dialog.dismiss();
+                        }
+                        dialog.dismiss();
+                    }
+                });
+
+
+                dialog.show();
+            }
+        });
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -109,5 +193,10 @@ public class MainActivity extends AppCompatActivity {
     public void clear() {
         this.dataList.clear();
         dataListAdapter.notifyDataSetChanged();
+    }
+
+    public ObjectData getData(int id) {
+
+        return realm.where(ObjectData.class).equalTo("id", id).findFirst();
     }
 }
